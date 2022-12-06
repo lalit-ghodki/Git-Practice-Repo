0a5901bd17d96c35b2b4a72ff7a2ad0e6b6ae16c
Lalit Ghodki <lalitghodki2@gmail.com>
2022-12-06 16:59:19 +0530

    Mul.rb added by ram

---
+++ Mul.rb
@@ -0,0 +1,15 @@
+class Mul
+
+    def initialize(num1, num2)
+        @num1 = num1
+        @num2 = num2
+    end
+
+    def mul
+        puts "Mul of #{@num1} and #{@num2} is #{@num1*@num2}"
+    end
+    
+end
+
+obj = Mul.new(10,20)
+obj.mul
